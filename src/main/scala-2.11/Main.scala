c9a8bcff621c22412635674cc421614873745261
link5209 <195140532@qq.com>
2016-06-27 17:53:36 +0800

    mongo auth conn

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -2,9 +2,7 @@ import com.guanghe.MongoDB
 import org.mongodb.scala.model.Filters._
 
 object Main extends App {
-//  val mongo = MongoDB("""
-//    mongodb://root:unitedmaster@dds-bp1866b723420d142.mongodb.rds.aliyuncs.com:3717,
-//    dds-bp1866b723420d141.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-1109427""", "test")
+//  val mongo = MongoDB("""root:unitedmaster@dds-bp1866b723420d142.mongodb.rds.aliyuncs.com:3717, dds-bp1866b723420d141.mongodb.rds.aliyuncs.com:3717/?authSource=admin&replicaSet=mgset-1109427""", "test")
 
   val mongo = MongoDB("10.8.8.8:27017", "test")
 
